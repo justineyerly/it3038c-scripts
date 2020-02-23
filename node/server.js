a89f667144b871b04841f0199ec408debe540018
uc-eyerlyja <eyerlyja@mail.uc.edu>
2020-02-23 16:55:11 -0500

    Updated files

--- node/server.js
+++ node/server.js
@@ -1,9 +1,17 @@
 var http = require("http");
+var fs = require("fs");
 
-var server = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type":"text/html"});
-    res.end("Hello from Node!");
-});
+http.createServer(function(req, res){
 
-server.listen(3000); 
+    if(req.url ==="/") {
+        fs.readFile("./Public/index.html","UTF-8", function(err, body){
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.end(body);
+        });
+    }
+        else{
+            res.writeHead(404, {"Content-Type": "text/plain"});
+            res.end(`404 File Not Found at ${req.url}`);
+        }
+}).listen(3000);
 console.log("Server Listening on port 3000");
